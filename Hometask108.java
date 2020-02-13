30c50f1a48393232cc605f38dfd2a80435b9368a
MatvejTk <60775516+MatvejTk@users.noreply.github.com>
2020-02-13 16:10:42 +0300

    Update Hometask108.java

--- Hometask108.java
+++ Hometask108.java
@@ -8,7 +8,7 @@ public static void main (String[] args)
 	a = 3;
 	b = 18;
 	c = 3;
-	rav = (b+Math.sqrt(b*b+4*a*c))/(2*a)-Math.pow(a, 3) * c + 1 / Math.pow(b, 2);
-		System.out.println("Значение - " + rav);
+	znach = (b+Math.sqrt(b*b+4*a*c))/(2*a)-Math.pow(a, 3) * c + 1 / Math.pow(b, 2);
+		System.out.println("Значение - " + znach);
     }
 }
