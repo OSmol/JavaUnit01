83b9ebce78d440f6535fc3a629209e7f83125fd3
MatvejTk <60775516+MatvejTk@users.noreply.github.com>
2020-03-20 05:16:11 +0300

    Update Hometask115.java

--- Hometask115.java
+++ Hometask115.java
@@ -2,6 +2,9 @@ package unit1;
 
 public class Hometask115 {
     public static  void main(String[] args) { 
-        System.out.format("Число Пи %.4f\n", Math. PI); 
-    } 
+        double pi = 3.14;
+        int power = 4; // 4-я степень
+        double pi4 = Math.pow(pi, power);
+        		System.out.println("4-я степень числа Пи равна: " + pi4);
+    }
 }
