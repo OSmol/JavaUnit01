c77452fc12332c2b0ba0ab2ed72204b0a16bb0ce
MatvejTk <60775516+MatvejTk@users.noreply.github.com>
2020-03-20 05:16:50 +0300

    Update Hometask110.java

--- Hometask110.java
+++ Hometask110.java
@@ -1,5 +1,22 @@
 package unit1;
 
 public class Hometask110 {
+public static void main(String[] args) {
+double x = 5.3;
+double y = 8.6;
+double a, b, c, d, e, total;
 
+a = Math.sin(x);
+b = Math.cos(x);
+c = Math.sin(y);
+d = Math.cos(y);
+e = Math.tan(x * y);
+total = ((a + d) / (b - c)) * e;
+if (b - c != 0) {
+total = ((a + d) / (b - c)) * e;
+System.out.println("Значение выражения = " + total);
+}
+else {System.out.println("Знаменатель равен нулю");
+}
+}
 }
